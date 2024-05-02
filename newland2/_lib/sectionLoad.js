e702ed6fa9b2786ef1bf0f3c43520e6d4d809fca
KhVeMx <grassandping@foxmail.com>
2024-05-02 15:34:53 +0800

    v1.3.6
    - 完善 css timeline
    - 完善 js 加载页面

--- newland2/_lib/sectionLoad.js
+++ newland2/_lib/sectionLoad.js
@@ -6,11 +6,18 @@ function loadSection(contentUrl) {
     }).then(function(htmltext) {
         document.getElementById('section').innerHTML = htmltext;
         // 使用HTML5 History API的 pushState 方法更新浏览器的地址栏，同时不重新加载页面。
-        history.pushState({ path: contentUrl }, '', contentUrl);
+        if(contentUrl == './ciallo.html'){
+            // ciallo页面是index的默认内容
+            history.pushState({ path: contentUrl }, '', 'index.html');
+        }
+        else {
+            history.pushState({ path: contentUrl }, '', contentUrl);
+        }
     }).catch(function(err) {
         console.warn('Component failed to load', err);
-    });
+    }); 
 }
+
 
 // 当页面加载后根据url中带有的page参数加载html内容到<div id="section">中
 document.addEventListener('DOMContentLoaded', function() {
@@ -27,7 +34,8 @@ const page = urlParams.get('page');
     }
 });
 
-// 监听导航点击事件 阻止页面重新加载 仅加载html文本到<div id="section">中
+
+// 点击事件 阻止页面重新加载 仅加载html文本到<div id="section">中
 document.body.addEventListener('click', function(event) {
     if (event.target.matches('#nav .alink')) {
         // 阻止链接默认行为
@@ -36,6 +44,18 @@ document.body.addEventListener('click', function(event) {
         // 阻止并根据#nav .alink中的href参数确定需要加载内容的对象
         const contentUrl = event.target.getAttribute('href') || './blank.html';
         // 调用loadSection直接加载href指定内容中的html文本，而不需要重新加载整个网页
+        loadSection(contentUrl);
+    }
+
+    if (event.target.matches('#section .timeline a')) {
+        event.preventDefault();
+        const contentUrl = event.target.getAttribute('href') || './blank.html';
+        loadSection(contentUrl);
+    }
+
+    if (event.target.matches('#header a')) {
+        event.preventDefault();
+        const contentUrl = event.target.getAttribute('href') || './blank.html';
         loadSection(contentUrl);
     }
 });
